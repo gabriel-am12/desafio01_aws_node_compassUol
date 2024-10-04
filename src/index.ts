1a5ae1a891ad7b16aa984c502d12ef7bcbf07ba6
gabriel acer <gabrielaug4@gmail.com>
2024-10-04 16:21:46 -0300

    feat: set Swagger middleware in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express, { Express, Request, Response, Application } from 'express';
 import dotenv from "dotenv";
 import router from "./routes/cars-router";
 import cors from "cors";
+import { swaggerUi, swaggerDocs } from './utils/swagger';
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+// Swagger middleware
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(cors());
